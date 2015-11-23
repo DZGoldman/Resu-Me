19e6327f16cb6bbb510e2e9d8dc4db2caca5105f
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-11-23 11:07:34 -0500

    userresume model

--- models/user_resume.js
+++ models/user_resume.js
@@ -1,1 +1,12 @@
 //user resume
+var mongoose = require('mongoose');
+
+var UserResumeSchema = new mongoose.Schema({
+  education: Array,
+  experiences: Array,
+  summary: Array
+})
+
+var UserResume = mongoose.model('UserResume', UserResumeSchema)
+
+module.exports = UserResume
