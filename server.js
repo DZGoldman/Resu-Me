b184c565173af166849633ad0bef74e1afadcd2d
echoechochamber <akwillia2@gmail.com>
2015-11-24 11:50:42 -0500

    server.js changes resume model commit

--- server.js
+++ server.js
@@ -5,33 +5,83 @@ var logger = require('morgan');
 var app = express();
 var fs = require('fs');
 // use an index.html
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 //app.use(express.static('public'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 // log with morgan
 app.use(logger('dev'));
 app.set('view engine', 'ejs');
 // connect to Mongodb
-mongoose.connect('mongodb://localhost/resu-me', (err)=>{
-   if(err){
+mongoose.connect('mongodb://localhost/resu-me', (err) => {
+   if (err) {
       console.log(err);
    } else {
       console.log('connection successfull');
    }
 });
 
-app.listen(3000, ()=>{ console.log("Resu-me listening on Port 3000")});
+app.listen(3000, () => {
+   console.log("Resu-me listening on Port 3000")
+});
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
    res.render('index', req.body);
 });
 
-app.get('/resumeform', function (req, res) {
-    res.render('resume');
-  });
+app.get('/resumeform', function(req, res) {
+   res.render('resumeForm', {
+      title: "Save a new ", //page title
+      action: "/resumeform", //post action for the form
+      fields: [{
+         name: "name",
+         type: 'text'
+         property: required
+      }, {
+         name: "streetAddress",
+         type: 'text'
+         property: required
+      }, {
+         name: "email",
+         type: 'text'
+         property: required
+      }, {
+         name: "phone",
+         type: 'text'
+         property: required
+      }, {
+         name: "summary",
+         type: 'text'
+         property: required
+      }, {
+         name: "experiences",
+         type: 'text'
+         property: required
+      }, {
+         name: "title",
+         type: 'text'
+         property: required
+      }, {
+         name: "startDate",
+         type: 'text'
+         property: required
+      }, {
+         name: "endDate",
+         type: 'text'
+         property: required
+      }, {
+         name: "description",
+         type: 'text'
+         property: required
+      }]
+   });
+});
+
+
 
 //
 // fs.readdirSync('./controllers').forEach(function (file) {
