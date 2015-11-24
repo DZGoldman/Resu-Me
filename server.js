aa3b90e0938196d21315f40041a1614cf47f8442
Kerstein Perez <kersteinperez@gmail.com>
2015-11-24 16:20:53 -0500

    start passport attempt, pull down

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var app = express();
 var fs = require('fs');
+//requirements for passport
+var passport = require('passport');
+var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var configDB = require('./config/database.js');
+
+
+// set up express app
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// required for pass port routes
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+
+
 // use an index.html
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -22,6 +45,9 @@ mongoose.connect('mongodb://localhost/resu-me', (err)=>{
       console.log('connection successfull');
    }
 });
+
+// require('./config/passport')(passport); // pass passport for configuration
+
 
 app.listen(3000, ()=>{ console.log("Resu-me listening on Port 3000")});
 
