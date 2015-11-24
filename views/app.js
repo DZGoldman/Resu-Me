3cdf8a918d4cb51fd908a2665020ae4dffeea939
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-11-24 13:53:44 -0500

    asdf

--- views/app.js
+++ views/app.js
@@ -3,13 +3,20 @@ console.log('hello dave');
 $(function () {
 
 //sign up
-
 // login
-
 //search box
+
+// on click of search,
+// send ajax request to data constroller
+    // .done, get the data, visualize it, put it one the index
+
+
 
 
 // write new resume
+// on click, render resume view, which will have a form.
+// button for form, on click, send ajax request to new resume controller
+
 
   $('#create-new-resume').on('click', function () {
     console.log('making new resume');
