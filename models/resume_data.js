c9e1295ed996ad181eee16989cd51c960a9ab598
echoechochamber <akwillia2@gmail.com>
2015-11-26 23:57:57 -0500

    preparing to merge seeding

--- models/resume_data.js
+++ models/resume_data.js
@@ -1,11 +1,38 @@
+// NOTE: '/((\S)+[\s(\S)]+(\S)+)/' is a regex matching expression
+// NOTE: that Requires there is AT LEAST one 'non-whitespace' character
+// NOTE: (Added because I noticed a couple had nothing in their)
+
 var mongoose = require('mongoose');
-//
+
+// validation Regex requires there's at least one non-whitespace character
+var validRegex = [/((\S)+[\s(\S)]+(\S)+)/, "This resume's `{PATH}` doesn't match the regex with `{VALUE}`"];
+
 var ResumeDataSchema = new mongoose.Schema({
-  title: String,
-  experiences: Array,
-  summary: String
+   title: String,
+   experiences: [{
+      type: String,
+      validate: validRegex,
+      required: true
+   }],
+   summary: {
+      type: String,
+      validate: validRegex,
+      required: true
+   }
 })
 
-var ResumeData = mongoose.model('ResumeData', ResumeDataSchema)
+var ResumeData = mongoose.model('ResumeData', ResumeDataSchema);
 
-module.exports = ResumeData
+//NOTE : validate that the array is longer than 0,
+// NOTE: because the hash only validates that things are bing passed in
+ResumeDataSchema.path('experiences').validate(function(experiences) {
+   if (!experiences) {
+      return false
+   } else if (experiences.length === 0) {
+      return false
+   }
+   return true;
+}, 'Resume needs to have at least one valid experiences');
+
+
+module.exports = ResumeData;
--- server.js
+++ server.js
@@ -4,35 +4,46 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var app = express();
 var fs = require('fs');
+var ResumeData = require('./models/resume_data.js');
+
 // use an index.html
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 //app.use(express.static('public'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.use(express.static(__dirname + '/public'))
-// log with morgan
+   // log with morgan
 app.use(logger('dev'));
 app.set('view engine', 'ejs');
+
 // connect to Mongodb
-mongoose.connect('mongodb://localhost/resu-me', (err)=>{
-   if(err){
+mongoose.connect('mongodb://localhost/resu-me', (err) => {
+   if (err) {
       console.log(err);
    } else {
       console.log('connection successfull');
    }
 });
 
-app.listen(3000, ()=>{ console.log("Resu-me listening on Port 3000")});
+app.listen(3000, () => {
+   console.log("Resu-me listening on Port 3000")
+});
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
    res.render('index', req.body);
 });
 
-app.get('/resumeform', function (req, res) {
-    res.render('resume');
-  });
+app.get('/test', function(req, res) {
+   ResumeData.find({
+      title: "computer programmer"
+   }).exec(function(err, resumes) {
+      res.send(resumes);
+   });
+});
 
 //
 // fs.readdirSync('./controllers').forEach(function (file) {
