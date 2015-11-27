5820c58a5e9f3726bbad3ee8bc7eb49094fb1301
Kerstein Perez <kersteinperez@gmail.com>
2015-11-27 13:51:42 -0500

    Friday commit

--- routes.js
+++ routes.js
@@ -52,7 +52,7 @@ module.exports = function(app, passport) {
 
 
     // LOGOUT ==============================
-  
+
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
