e46218d0bf17e4de17873059e7c98a5e38f508fe
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-11-27 17:23:03 -0500

    removed redundant '/' route in routes

--- routes.js
+++ routes.js
@@ -3,9 +3,9 @@ module.exports = function(app, passport) {
 
     // HOME PAGE (with login links) ========
 
-    app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
-    });
+    // app.get('/', function(req, res) {
+    //     res.render('index.ejs'); // load the index.ejs file
+    // });
 
 
     // LOGIN ===============================
