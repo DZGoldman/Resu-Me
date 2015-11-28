f96f98f62fcab3e58591f09da4cf439786948ad7
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-11-28 07:46:58 -0500

    refactored app.js

--- public/app.js
+++ public/app.js
@@ -3,44 +3,25 @@ console.log('hello dave');
 $(function () {
 
   var clickCount =1;
-  $('#Experience_Button').click(function () {
-    console.log( 'clicked');
+  $('#Experience_Button').click(addExperienceField);
 
-    var $copy = $($('#experience-template').html()).clone();
-  $('#experience-container').append($copy)
-  });
+  $('#Education_Button').click(addEducationField);
+})
 
-  $('#Education_Button').click(function () {
-    console.log( 'clicked');
+//new resume form button helpers:
+var addExperienceField= function () {
+  var $copy = $($('#experience-template').html()).clone();
+$('#experience-container').append($copy)
+}
 
-    var $copy = $($('#education-template').html()).clone();
-    console.log($copy);
-  $('#education-container').append($copy)
-  });
+var addEducationField =function () {
+  var $copy = $($('#education-template').html()).clone();
+  console.log($copy);
+$('#education-container').append($copy)
+}
 
-
-
-//sign up
-// login
 //search box
 
 // on click of search,
 // send ajax request to data constroller
     // .done, get the data, visualize it, put it one the index
-
-
-
-
-// write new resume
-// on click, render resume view, which will have a form.
-// button for form, on click, send ajax request to new resume controller
-
-
-
-  //submit a new resume
-
-
- // upload a resume
-
-
-})
