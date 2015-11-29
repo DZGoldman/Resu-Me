25d278be5721b0241d3fdcf52b8bb64cf907098e
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-11-29 18:28:24 -0500

    edit works except it takes a rrefresh for the old one to disappear

--- controllers/userResumeController.js
+++ controllers/userResumeController.js
@@ -10,25 +10,32 @@ var User = require('../models/user.js')
 //app.post resume/:resumeparams
 //make a new Resume with params from form, try to save
 
-module.exports.controller = function (app) {
+module.exports.controller = function(app) {
 
   function isLoggedIn(req, res, next) {
-      // if user is authenticated in the session, carry on
-      if (req.isAuthenticated())
-          return next();
-      // if they aren't redirect them to the home page
-      res.redirect('/');
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+      return next();
+    // if they aren't redirect them to the home page
+    res.redirect('/');
   }
 
 
-  app.get('/newresume', isLoggedIn, function (req, res) {
-    var foundthing={education:[], experiences: []};
-     res.render('new_resume', {req:req, foundthing: foundthing})
+  app.get('/newresume', isLoggedIn, function(req, res) {
+    var foundthing = {
+      education: [],
+      experiences: []
+    };
+    res.render('new_resume', {
+      req: req,
+      foundthing: foundthing
+    })
   })
 
-  app.post('/newresume', function (req,res) {
+  app.post('/newresume', function(req, res) {
     //sub == 'submission'
-    var sub= req.body
+    var sub = req.body
+      //start new function
     var newResume = new UserResume();
     //make a new resume, give all submission data to its appropriate keys
     newResume.name = sub.Name;
@@ -37,98 +44,183 @@ module.exports.controller = function (app) {
     newResume.phone = sub.Phone;
     newResume.education = sub.Education
     newResume.summary = sub.Summary
-    //create a new experience object for the array of experience for each new experience that gets added
+      //create a new experience object for the array of experience for each new experience that gets added
     newResume.experiences = [];
     var experienceObject = {}
-    sub.textinput.forEach(function (input, index) {
-      switch(index%4){
-        case 0:
-        experienceObject.title = input
-        break;
-        case 1:
-        experienceObject.startDate = input;
-        break;
-        case 2:
-        experienceObject.endDate = input;
-        break;
-        case 3:
-        experienceObject.description = input;
-        newResume.experiences.push(experienceObject)
-        break;
-      }
-    })
-    //give new resume to the current user
+    sub.textinput.forEach(function(input, index) {
+        switch (index % 4) {
+          case 0:
+            experienceObject.title = input
+            break;
+          case 1:
+            experienceObject.startDate = input;
+            break;
+          case 2:
+            experienceObject.endDate = input;
+            break;
+          case 3:
+            experienceObject.description = input;
+            newResume.experiences.push(experienceObject)
+            break;
+        }
+      })
+      //give new resume to the current user
     req.user.resumes.push(newResume);
     //save unpdated current user and the resume itself into the db
     newResume.save(function(err) {
-        if (err)
-            throw err;
+      if (err)
+        throw err;
     });
     req.user.save(function(err) {
-        if (err)
-            throw err;
-    });
-    res.render('profile', {req: req, user : req.user})
+      if (err)
+        throw err;
+    }); //end new function
+    res.render('profile', {
+      req: req,
+      user: req.user
+    })
   })
 
-  app.get('/resume/edit/:id', function (req, res) {
+  app.get('/resume/edit/:id', function(req, res) {
     //  var UserResume.findById(req.params.id);
     var resumeID = req.params.id;
     console.log(resumeID);
-    UserResume.findById(resumeID, function (err, foundthing) {
+    UserResume.findById(resumeID, function(err, foundthing) {
       if (err) {
         console.log(err);
-      }else {
-        console.log('sucess:',foundthing.summary);
+      } else {
+        console.log('sucess:', foundthing.summary);
         console.log(foundthing);
 
-        res.render('new_resume', {req: req, foundthing: foundthing})
+        res.render('new_resume', {
+          req: req,
+          foundthing: foundthing
+        })
       }
+
+    })
+      });
+
+    app.post('/updateresume', function(req, res) {
+      console.log('updating a resume');
+      var sub = req.body
+        //delete the old resume (from user and database)
+      var resumeID = sub.ID;
+      console.log(sub.ID);
+      //delete function:
+      UserResume.remove({
+          _id: resumeID
+        }, function(err, result) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('deleted');
+            var userID = req.user._id;
+            User.findById(userID, function(err, user) {
+              if (err) throw err;
+              user.resumes.forEach(function(resume, index) {
+                if (resume._id == resumeID) {
+                  user.resumes.splice(index, 1);
+                  return
+                }
+              })
+              user.save(function(err) {
+                if (err) throw err;
+              })
+            })
+          }
+        }) //end delete function
+
+      var sub = req.body
+        //start new function
+      var newResume = new UserResume();
+      //make a new resume, give all submission data to its appropriate keys
+      newResume.name = sub.Name;
+      newResume.streetAddress = sub.Address;
+      newResume.email = sub.Email;
+      newResume.phone = sub.Phone;
+      newResume.education = sub.Education
+      newResume.summary = sub.Summary
+        //create a new experience object for the array of experience for each new experience that gets added
+      newResume.experiences = [];
+      var experienceObject = {}
+      sub.textinput.forEach(function(input, index) {
+          switch (index % 4) {
+            case 0:
+              experienceObject.title = input
+              break;
+            case 1:
+              experienceObject.startDate = input;
+              break;
+            case 2:
+              experienceObject.endDate = input;
+              break;
+            case 3:
+              experienceObject.description = input;
+              newResume.experiences.push(experienceObject)
+              break;
+          }
+        })
+        //give new resume to the current user
+      req.user.resumes.push(newResume);
+      //save unpdated current user and the resume itself into the db
+      newResume.save(function(err) {
+        if (err)
+          throw err;
+      });
+      req.user.save(function(err) {
+        if (err)
+          throw err;
+      }); //end new function
+
+      res.redirect('/profile')
 
     })
 
 
 
-  });
 
-  app.get('/resume/delete/:id', function (req, res) {
+  app.get('/resume/delete/:id', function(req, res) {
     var resumeID = req.params.id
-    UserResume.remove({_id: resumeID}, function (err, result) {
-      if (err) {
-        console.log(err);
-      }else {
-        console.log('deleted');
-        var userID = req.user._id;
-        User.findById(userID,function (err,user) {
-          if (err) throw err;
-          user.resumes.forEach(function (resume, index) {
-            if (resume._id==resumeID) {
+      //delete function:
+    UserResume.remove({
+        _id: resumeID
+      }, function(err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('deleted');
+          var userID = req.user._id;
+          User.findById(userID, function(err, user) {
+            if (err) throw err;
+            user.resumes.forEach(function(resume, index) {
+              if (resume._id == resumeID) {
                 user.resumes.splice(index, 1);
                 return
               }
+            })
+            user.save(function(err) {
+              if (err) throw err;
+            })
           })
-          user.save(function (err) {
-            if (err) throw err;
-          })
-        })
-      }
-    })
+        }
+      }) //end delete function
     res.redirect('/profile')
   })
 
-  app.get('/resume/makecurrent/:id',function (req,res) {
+  app.get('/resume/makecurrent/:id', function(req, res) {
     var resumeID = req.params.id
     var userID = req.user._id;
-    User.findById(userID,function (err,user) {
+    User.findById(userID, function(err, user) {
       if (err) throw err;
-      user.resumes.forEach(function (resume, index) {
-        if (resume._id==resumeID) {
-            user.resumes.splice(index, 1);
-            user.resumes.push(resume)
-            return
-          }
+      user.resumes.forEach(function(resume, index) {
+        if (resume._id == resumeID) {
+          user.resumes.splice(index, 1);
+          user.resumes.push(resume)
+          return
+        }
       })
-      user.save(function (err) {
+      user.save(function(err) {
         if (err) throw err;
       })
     })
