d03bfff2ccb5fd16123d0f99019f05c7fb31b834
echoechochamber <akwillia2@gmail.com>
2015-11-30 04:00:18 -0500

    it LIIIIIVVVVVVESSSSS!!!!! *thunderstrike*

--- public/app.js
+++ public/app.js
@@ -1,11 +1,15 @@
 $(function() {
    /*********  Visualization  *************/
+   // create a wordcloud when the button is hit
+   $('button#cloudQuery').click(function() {
+      getCloudData();
+   });
    // testing function to display the word cloud
    $('button#search-query').click(getCloudData);
-$('#analyze-resume').click(function(){
-$('#analyze-resume').css("background-color", "red");
-   getClassify() ;
-});
+   $('#analyze-resume').click(function() {
+      $('#analyze-resume').css("background-color", "red");
+      getClassify();
+   });
 
    //sign up
    // login
@@ -19,38 +23,32 @@ $('#analyze-resume').css("background-color", "red");
    $('#create-new-resume').on('click', function() {
       console.log('making new resume');
       $.get('/resumeform') // render ejs
-   })
+   });
 
-})
-   var fullText = function (resume){
-      var result = [];
-      result.push(resume.summary);
-      resume.experiences.forEach( function(value, index){
-         result.push(value.description);
-      });
-      return result.join(' ');
-   };
+});
+
+var fullText = function(resume) {
+   var result = [];
+   result.push(resume.summary);
+   resume.experiences.forEach(function(value, index) {
+      result.push(value.description);
+   });
+   return result.join(' ');
+};
 
 
 var testQuery = 'computer programmer/';
 
-   // create a wordcloud when the button is hit
-   $('button#cloudQuery').click(function() {
-      getCloudData();
-   });
-});
-
-
 /*******  Resume Form *********/
-var addExperienceField= function () {
-  var $copy = $($('#experience-template').html()).clone();
-$('#experience-container').append($copy)
+var addExperienceField = function() {
+   var $copy = $($('#experience-template').html()).clone();
+   $('#experience-container').append($copy)
 }
 
-var addEducationField =function () {
-  var $copy = $($('#education-template').html()).clone();
-  console.log($copy);
-$('#education-container').append($copy)
+var addEducationField = function() {
+   var $copy = $($('#education-template').html()).clone();
+   console.log($copy);
+   $('#education-container').append($copy)
 }
 
 
@@ -113,31 +111,88 @@ var showCloud = function(data) {
 
 
 // comparing user resumes
-var getClassify = function(){
+var getClassify = function() {
    $('#analyze-resume').css("background-color", "blue");
    $.get('/compareResume').done(classifyAndCompare);
    // take the current resume of the current user
    // send ajax request to the classifiction route
 }
 
-var classifyAndCompare = function(user){
+var classifyAndCompare = function(user) {
    $('#analyze-resume').css("background-color", "yellow");
-var currentResume = user.resumes[user.resumes.length-1];
-var full = fullText(currentResume);
-
-// FIXME change this and the route in server.js to ensure
-$.get('/analyzeResume/'+testQuery + full).done(showClassification);
+   var currentResume = user.resumes[user.resumes.length - 1];
+   var full = fullText(currentResume);
+   var q = $("#cloud-vis").val() + '/';
+   // FIXME change this and the route in server.js to ensure
+   $.get('/analyzeResume/' + q + full).done(showClassification);
 }
 
 // this will just be a d3 function that will take all of the data and display it
-var showClassification = function(classification){
+var showClassification = function(classification) {
+   $(".graphic").remove();
    $('#analyze-resume').css("background-color", "green");
-//classifications is an array of hashes with the label and the value
-// it is returned sorted by likeess
+   //classifications is an array of hashes with the label and the value
+   // it is returned sorted by likeess
 
-console.log(classification);
-classification.forEach(function(val, index){
-   var add = $('<p>').text(val.label + ": " + val.value);
-   $('.container').append( add );
-})
+   /* NOTE test functions
+   console.log(classification);
+   classification.forEach(function(val, index){
+      var add = $('<p>').text(val.label + ": " + val.value);
+      $('.container').append( add );
+   })
+   NOTE end of test functions*/
+
+   // Setup the D3 svg
+   var w = 800;
+   var h = 200;
+   var m = 1000000;
+   var pad = 3;
+   var len = classification.length;
+
+   // normalize all of the values.
+   var min = d3.min(classification, function(d) {
+      return d.value;
+   });
+
+   var max = d3.max(classification, function(d) {
+      return d.value;
+   });
+   var yScale = d3.scale.log()
+      .domain([min, max])
+      .range([0, h]);
+
+   var svg = d3.select("div#graph")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h)
+      .attr("class", "graphic");
+
+
+   svg.selectAll("rect")
+      .data(classification)
+      .enter()
+      .append("rect")
+      .attr("x", function(d, i) {
+         return i * (w / classification.length);
+      })
+      .attr("y", function(d) {
+         return h - yScale(d.value);
+      })
+      .attr('width', (w / classification.length - pad))
+      .attr('height', function(d) {
+         // console.log(d.value)
+         return yScale(d.value);
+      })
+      .style("fill", "blue")
+      .on("hover", function(){
+         d3.select(this).style("fill", "orange");
+      })
+      .on("mouseover", function(d) {
+         $('#job-description').text(d.label);
+      })
+      .on("mouseout", function() {
+         $('#job-description').text('___________________');
+      });
+   // // create the
+   // debugger;
 }
